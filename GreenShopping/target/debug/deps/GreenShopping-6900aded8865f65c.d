aec0c50e140c44b5630b7613c01d375fd3145e39
Ömer Faruk Yıldız <146623069+omefeye@users.noreply.github.com>
2025-05-18 11:15:49 +0300

    Delete GreenShopping directory

--- GreenShopping/target/debug/deps/GreenShopping-6900aded8865f65c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dnsyl\Documents\GitHub\16MayisBozokHackathon\Stellar-BozokHackathon\GreenShopping\target\debug\deps\libGreenShopping-6900aded8865f65c.rmeta: src\main.rs
-
-C:\Users\dnsyl\Documents\GitHub\16MayisBozokHackathon\Stellar-BozokHackathon\GreenShopping\target\debug\deps\GreenShopping-6900aded8865f65c.d: src\main.rs
-
-src\main.rs:
--- GreenShopping/target/debug/deps/GreenShopping-f6f26537485dc90b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dnsyl\Documents\GitHub\16MayisBozokHackathon\Stellar-BozokHackathon\GreenShopping\target\debug\deps\libGreenShopping-f6f26537485dc90b.rmeta: src\main.rs
-
-C:\Users\dnsyl\Documents\GitHub\16MayisBozokHackathon\Stellar-BozokHackathon\GreenShopping\target\debug\deps\GreenShopping-f6f26537485dc90b.d: src\main.rs
-
-src\main.rs:
